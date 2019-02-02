e4947321684eda6fd52386f9a8afef4c3835f26f
John Lee <jlee14c@hotmail.com>
2019-02-01 20:42:34 -0800

    updated

---
+++ src/family/family.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100%',
+    width: '100%',
+    border: '1px solid black'
+  }
+};
+
+class FamilyScreen extends Component {
+  render() {
+    return (
+      <div style={styles.container}>
+        <h1>Hello World</h1>
+      </div>
+    );
+  }
+}
+
+export default FamilyScreen;
