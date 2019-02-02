654ae4e4659c8a7eee5492ada52087daf82e77d5
John Lee <jlee14c@hotmail.com>
2019-02-01 20:49:35 -0800

    added css sheets

--- src/member/member.js
+++ src/member/member.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './member.css';
 
 const styles = {
   container: {
--- src/splash/splash.js
+++ src/splash/splash.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './splash.css';
 
 const styles = {
     container: {
