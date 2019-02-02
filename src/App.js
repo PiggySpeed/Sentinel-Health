213822bd3624a5cc58ac9d17823444585790a65c
Steven <stevennhnk113@gmail.com>
2019-02-02 06:28:13 -0800

    Able to send email

--- src/App.js
+++ src/App.js
@@ -7,16 +7,26 @@ import Member from "./member/member.js";
 import Device from "./device/device.js";
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <Splash />
-        {/*<Family />*/}
-        {/*<Member />*/}
-        {/*<Device />*/}
-      </div>
-    );
-  }
+
+	sendEmail() {
+		// Temporary sending to a local server, will spin a real one when I wake up, if necessary
+		const url = "http://localhost:8000/send"; // site that doesn’t send Access-Control-*
+		fetch(url)
+			.then(response => response.text())
+			.then(contents => console.log(contents))
+			.catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+	}
+
+	render() {
+		return (
+			<div className="App" >
+				<Splash />
+				{/*<Family />*/}
+				{/*<Member />*/}
+				{/*<Device />*/}
+			</div>
+		);
+	}
 }
 
 export default App;
